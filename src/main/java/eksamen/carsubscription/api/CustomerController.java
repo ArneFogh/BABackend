896b6333c56e0e8856b5ca0bb034dbcc2efe0692
madsen10 <91535029+madsen10@users.noreply.github.com>
2023-11-30 13:23:07 +0100

    API CORS = 3000

--- src/main/java/eksamen/carsubscription/api/CustomerController.java
+++ src/main/java/eksamen/carsubscription/api/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-
+@CrossOrigin(origins = "http://localhost:3000")
 public class CustomerController {
 private CustomerService customerService;
 
