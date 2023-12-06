4af5a98f8d281f688d1ba53e542a3118987b6984
nicklassienczak <nicklas-sk@hotmail.com>
2023-12-06 15:00:01 +0100

    Changed CrossOrigin to Azure

--- src/main/java/eksamen/carsubscription/api/CustomerController.java
+++ src/main/java/eksamen/carsubscription/api/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://babackenddbapi.azurewebsites.net")
 public class CustomerController {
 private CustomerService customerService;
 
--- src/main/java/eksamen/carsubscription/api/LeaseAgreementController.java
+++ src/main/java/eksamen/carsubscription/api/LeaseAgreementController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://babackenddbapi.azurewebsites.net")
 @RestController
 public class LeaseAgreementController {
 
