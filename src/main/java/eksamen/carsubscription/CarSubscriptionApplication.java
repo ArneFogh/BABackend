b7c48721b7e36f60f1c06b1a66a29dac5120b796
nicklassienczak <nicklas-sk@hotmail.com>
2023-12-11 18:15:06 +0100

    CD fix?

--- src/main/java/eksamen/carsubscription/CarSubscriptionApplication.java
+++ src/main/java/eksamen/carsubscription/CarSubscriptionApplication.java
@@ -1,11 +1,9 @@
 package eksamen.carsubscription;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication (exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class CarSubscriptionApplication {
 
     public static void main(String[] args) {
