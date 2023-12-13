20f70db7e74f0eab94d2e7c5a15fbd39c0dd518f
madsen10 <91535029+madsen10@users.noreply.github.com>
2023-12-13 20:40:59 +0100

    CORS URL change

--- src/main/java/eksamen/carsubscription/services/LeaseAgreementService.java
+++ src/main/java/eksamen/carsubscription/services/LeaseAgreementService.java
@@ -1,7 +1,14 @@
 package eksamen.carsubscription.services;// LeaseAgreementService.java
+import eksamen.carsubscription.entity.Car;
+import eksamen.carsubscription.entity.Customer;
 import eksamen.carsubscription.entity.LeaseAgreement;
+import eksamen.carsubscription.entity.PickupLocation;
+import eksamen.carsubscription.repository.ICar;
+import eksamen.carsubscription.repository.ICustomer;
 import eksamen.carsubscription.repository.ILeaseAgreement;
+import eksamen.carsubscription.repository.IPickupLocation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,11 +17,17 @@ import java.util.Optional;
 @Service
 public class LeaseAgreementService {
 
-    // Antager, at du har et repository eller en service til LeaseAgreement
-    private ILeaseAgreement leaseAgreementRepository;
 
-    public LeaseAgreementService(ILeaseAgreement leaseAgreementRepository) {
+    private ILeaseAgreement leaseAgreementRepository;
+    private ICar carRepository;
+    private IPickupLocation pickupLocationRepository;
+    private ICustomer customerRepository;
+
+    public LeaseAgreementService(ILeaseAgreement leaseAgreementRepository, ICar carRepository, IPickupLocation pickupLocationRepository, ICustomer customerRepository) {
         this.leaseAgreementRepository = leaseAgreementRepository;
+        this.carRepository = carRepository;
+        this.pickupLocationRepository = pickupLocationRepository;
+        this.customerRepository = customerRepository;
     }
 
     public LeaseAgreement getLeaseAgreement(Long id) {
@@ -27,9 +40,57 @@ public class LeaseAgreementService {
 
 
 
-    public LeaseAgreement createLeaseAgreement(LeaseAgreement leaseAgreement) {
-        return leaseAgreementRepository.save(leaseAgreement);
+    public LeaseAgreement createLeaseAgreement(LeaseAgreement leaseAgreement) throws ChangeSetPersister.NotFoundException {
+        // Check om bilen eksisterer ved at bruge bilens id
+        Car car = leaseAgreement.getCar();
+        if (car != null && car.getCarId() != null) {
+            Optional<Car> carOptional = carRepository.findById(car.getCarId());
+
+            if (carOptional.isPresent()) {
+                // Hvis bilen findes, sæt bilen på leasingaftalen
+                leaseAgreement.setCar(carOptional.get());
+
+                // Check om afhentningsstedet eksisterer ved at bruge afhentningsstedets id
+                PickupLocation pickupLocation = leaseAgreement.getPickupLocation();
+                if (pickupLocation != null && pickupLocation.getId() != null) {
+                    Optional<PickupLocation> pickupLocationOptional = pickupLocationRepository.findById(pickupLocation.getId());
+
+                    if (pickupLocationOptional.isPresent()) {
+                        // Hvis afhentningsstedet findes, sæt det på leasingaftalen
+                        leaseAgreement.setPickupLocation(pickupLocationOptional.get());
+                    } else {
+                        // Håndter scenariet, hvor afhentningsstedet ikke findes
+                        throw new ChangeSetPersister.NotFoundException();
+                    }
+                }
+
+                // Check om kunden eksisterer ved at bruge kundens id
+                Customer customer = leaseAgreement.getCustomer();
+                if (customer != null && customer.getCustomerID() != null) {
+                    Optional<Customer> customerOptional = customerRepository.findById(customer.getCustomerID());
+
+                    if (customerOptional.isPresent()) {
+                        // Hvis kunden findes, sæt kunden på leasingaftalen
+                        leaseAgreement.setCustomer(customerOptional.get());
+                    } else {
+                        // Håndter scenariet, hvor kunden ikke findes
+                        throw new ChangeSetPersister.NotFoundException();
+                    }
+                }
+
+                // Gem leasingaftalen i databasen
+                return leaseAgreementRepository.save(leaseAgreement);
+            } else {
+                // Håndter scenariet, hvor bilen ikke findes
+                throw new ChangeSetPersister.NotFoundException();
+            }
+        } else {
+            // Håndter scenariet, hvor bilen eller dens id er null
+            throw new IllegalArgumentException("Car or car ID must not be null");
+        }
     }
+
+
 
     public LeaseAgreement updateLeaseAgreement(Long id, LeaseAgreement updatedLeaseAgreement) {
         Optional<LeaseAgreement> existingLeaseAgreement = leaseAgreementRepository.findById(id);
