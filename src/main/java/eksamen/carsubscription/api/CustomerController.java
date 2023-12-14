33959dab273204d7e801fb4f445f8adc7e7d636e
madsen10 <91535029+madsen10@users.noreply.github.com>
2023-12-14 17:11:44 +0100

    CORS change

--- src/main/java/eksamen/carsubscription/api/CustomerController.java
+++ src/main/java/eksamen/carsubscription/api/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "https://gust9196.github.io/frontend")
+@CrossOrigin(origins = "https://gust9196.github.io")
 public class CustomerController {
 private CustomerService customerService;
 
